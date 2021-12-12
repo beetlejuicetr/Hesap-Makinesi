3c54707b009412dc20970919716aae14caffe8f3
beetlejuicetr <64163239+beetlejuicetr@users.noreply.github.com>
2021-12-12 21:55:23 +0300

    Main update

---
+++ hesap_makinesi.py
@@ -0,0 +1,37 @@
+print("""
+
+    -----------------------------
+    |Hesap Makinesine Hoşgeldin!|
+    |                           |
+    |Sürüm   : v1.2             |
+    |Yapımcı : MID              |
+    |Dil     : Python           |
+    -----------------------------
+
+    """)
+
+print("""
+    *Nasıl Kullanılır?:
+    ---------------------------------
+    Hesap makinesini kullanmak
+    oldukça basitdir.
+    Örnegin bir kaç sayıyı toplama
+    ihtiyacı duyuyorsun veya
+    daha karmaşık işlemler yapmak
+    istiyorsun. Bunun için
+    sadece işlemi yazman yeterli. :)
+
+    Çıkmak için ' cıkıs ' yazman gerekiyor.
+    ---------------------------------
+    """)
+
+while True:
+    islem = input("İşlem: ")
+    if islem == "cıkıs":
+        print("Çıkış Yapılıyor...")
+        break
+    try:
+        print(eval(islem))
+    except :
+        print("Sadece sayı girmelisin!")
+
